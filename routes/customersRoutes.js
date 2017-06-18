000a80860c6fb7de3c8036515926131088227ed4
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2017-06-18 17:31:12 -0300

    Created Customer Controller
    Why?
    Has been created the customer controller and include the controller functions in it. Changed the routes file to call the controller functions.

--- routes/customersRoutes.js
+++ routes/customersRoutes.js
@@ -1,29 +1,15 @@
 let express = require('express');
 let router = express.Router();
+let CustomerController = require('../controllers/CustomerController');
 
-router.get('/', (req, res) => {
-    res.json({ name: 'Get all customers'});
-    res.status(200);
-});
+router.get('/', CustomerController.getAll);
 
-router.get('/:_id', (req, res) => {
-    res.json({ name: `Get customer with ID: ${req.param('_id')}`});
-    res.status(200);
-});
+router.get('/:_id', CustomerController.getById);
 
-router.post('/', (req, res) => {
-    res.json({ name: 'Save customer'});
-    res.status(201);
-});
+router.post('/', CustomerController.save);
 
-router.put('/:_id', (req, res) => {
-    res.json({ name: `Update customer with ID: ${req.param('_id')}`});
-    res.status(200);
-});
+router.put('/:_id', CustomerController.update);
 
-router.delete('/:_id', (req, res) => {
-    res.json({ name: `Delete customer with ID: ${req.param('_id')}`});
-    res.status(200);
-});
+router.delete('/:_id', CustomerController.remove);
 
 module.exports = router;
